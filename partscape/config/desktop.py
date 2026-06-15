129a724b91cac2234a86212426d72e27dd2cb5e1
Jeriel Francis <francisbeatsproductions@gmail.com>
2026-06-15 10:28:15 -0400

    chore(desktop): remove PartScape desktop icon

--- partscape/config/desktop.py
+++ partscape/config/desktop.py
@@ -2,12 +2,4 @@ from frappe import _
 
 
 def get_data():
-    return [
-        {
-            "module_name": "PartScape",
-            "color": "#1abc9c",
-            "icon": "octicon octicon-tools",
-            "type": "module",
-            "label": _("PartScape"),
-        }
-    ]
+    return []
